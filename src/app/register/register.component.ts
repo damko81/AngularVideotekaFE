71e0dca6a1cb4e90d9418ae81609f6bddd1c8629
Damjan <damko81@gmail.com>
2024-04-07 22:17:23 +0200

    Add confirm password into the reg form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,29 +11,38 @@ export class RegisterComponent {
   name: string ="";
   username: string ="";
   password: string ="";
+  passwordConf: string = "";
   public invalidRegist: boolean = false;
   public message : string = "";
 
   constructor(private http: HttpClient, private router: Router){ }
 
   save(){
-    let bodyData = {
-      "name" : this.name,
-      "username" : this.username,
-      "password" : this.password
-    };
-    this.http.post(`http://localhost:8080/users/save`,bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
-    {
-      if (resultData == "" || resultData == null){
-        this.invalidRegist = true;
-        this.message = "User Registered Faild";
-      }
-      else{  
-        
-            this.message = "User Registered Successfully";
-            this.router.navigate(['/login']);  
-      }
-    });
+
+    if (this.password != this.passwordConf){
+      this.message = "Confirm Password Invalid";
+      this.invalidRegist = true;
+    }
+    else{
+
+      let bodyData = {
+        "name" : this.name,
+        "username" : this.username,
+        "password" : this.password
+      };
+      this.http.post(`http://localhost:8080/users/save`,bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
+      {
+        if (resultData == "" || resultData == null){
+          this.invalidRegist = true;
+          this.message = "User Registered Faild";
+        }
+        else{  
+          
+              this.message = "User Registered Successfully";
+              this.router.navigate(['/login']);  
+        }
+      });
+    }
   }
 
 }
