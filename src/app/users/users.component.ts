dd59cfa7cf980732056fbb6050a21bed69bfda2c
Damjan <damko81@gmail.com>
2024-04-08 13:57:23 +0200

    Admin change password to user interface

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UsersService } from './users.service';
 import { Users } from './users';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -10,9 +10,17 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class UsersComponent implements OnInit {
 
+  @ViewChild('closebutton') closebutton: ElementRef | undefined
+
   public users?: Users[];
   public editUsers?: Users | null;
   public deleteUsers?: Users | null;
+  public invalidRegist: boolean = false;
+  public changePassword: boolean = false;
+  public invalidChangePassword: boolean = false;
+  public newPassword: string = "";
+  public newPasswordConf: string = "";
+  public message : string = "";
 
   constructor(private usersService: UsersService) { }
 
@@ -33,15 +41,28 @@ export class UsersComponent implements OnInit {
   }
 
   public onUpdateUsers(user: Users): void{
-    this.usersService.updateUsers(user).subscribe(
-      (response: Users) => {
-        console.log(response);
-        this.getUsers();
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
+ 
+    if (this.newPassword != this.newPasswordConf){
+       this.message = "Confirm New Password Invalid";
+       this.invalidChangePassword = true;
+    }
+    else{
+      this.closebutton?.nativeElement.click();
+      if(this.newPassword != ""){user.password=this.newPassword}
+      this.usersService.updateUsers(user).subscribe(
+        (response: Users) => {
+          console.log(response);
+          this.getUsers();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+   }
+  }
+
+  public onChangePassword(): void{
+     this.changePassword = true;
   }
 
   public onDeleteUsers(id?: number): void{
@@ -56,6 +77,14 @@ export class UsersComponent implements OnInit {
     );
   }
 
+  public resetChangedPassword(): void{
+    this.invalidChangePassword = false;
+    this.changePassword = false;
+    this.newPassword = "";
+    this.newPasswordConf = "";
+    this.message = "";
+  } 
+
   public onOpenModal(user: Users | null, mode?: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
@@ -66,6 +95,7 @@ export class UsersComponent implements OnInit {
       button.setAttribute('data-target', '#addUsersModal');
     }
     if (mode === 'edit') {
+      this.resetChangedPassword();
       this.editUsers = user;
       button.setAttribute('data-target', '#updateUsersModal');
     }
